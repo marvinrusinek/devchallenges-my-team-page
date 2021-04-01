30f1ebc570c8454b4a73f271fbdf55738a82b6e5
Marvin Rusinek <marvinrusinek@gmail.com>
2021-04-01 18:49:11 -0400

    initial upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './team-page/containers/container/container.component';
+import { HeaderComponent } from './team-page/containers/header/header.component';
+import { MainComponent } from './team-page/containers/main/main.component';
+import { FooterComponent } from './team-page/containers/footer/footer.component';
+import { LazyImgDirective } from './team-page/containers/main/lazy-img.directive';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'team-page',
+    pathMatch: 'full'
+  },
+  {
+    path: 'team-page',
+    loadChildren: () =>
+      import('./team-page/routing/teampage-routing.module').then(m => m.TeamPageRoutingModule)
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    LazyImgDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
